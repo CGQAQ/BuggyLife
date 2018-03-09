05ab3de7312727771e46ee78cb31646e232f5c94
CGQAQ <m.jason.liu@outlook.com>
2018-03-09 14:20:02 +0800

    Update README.mk

--- fedora install nvidia drivers/README.mk
+++ fedora install nvidia drivers/README.mk
@@ -1,1 +1,3 @@
-hello
+# This is just a backup for [if-not-true-then-false](https://www.if-not-true-then-false.com/2015/fedora-nvidia-guide/)
+这只是[if-not-true-then-false](https://www.if-not-true-then-false.com/2015/fedora-nvidia-guide/)的一个备份
+-
